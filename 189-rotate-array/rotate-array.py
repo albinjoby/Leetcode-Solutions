315fedc157e427c18a4a16145a49a18702291d30
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-08-10 20:10:42 +0530

    Time: 134 ms (98.60%) | Memory: 23.1 MB (54.03%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def rotate(self, nums, k):
-        # Mod k by lenght, so that we don't have to do extra rotations
+        # Mod k by lenght so that we don't have to do extra rotations
         k = k % len(nums)
         nums[:] = nums[-k:] + nums[:-k]

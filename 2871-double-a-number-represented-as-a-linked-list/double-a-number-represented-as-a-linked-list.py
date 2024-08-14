1ad12bbe2fd2d18a494911fdd4b5ed8c347be956
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-08-14 19:59:22 +0530

    Time: 722 ms (28.78%) | Memory: 22.9 MB (28.06%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -15,18 +15,29 @@ class Solution(object):
         nums = int(s)*2
         nums = str(nums)
         
-        i = 0
-        temp = head
+        # i = 0
+        # temp = head
+        # while i < len(nums):
+        #     val = int(nums[i])
+        #     if temp:
+        #         temp.val = val
+        #         if not temp.next and i < len(nums)-1:
+        #             temp.next = ListNode(0)
+        #         temp = temp.next
+        #     else:
+        #         temp = ListNode(val)
+        #     i += 1
+
+
+        # return head
+
+        new_head = ListNode(int(nums[0]))
+        i = 1
+        temp = new_head
         while i < len(nums):
-            val = int(nums[i])
-            if temp:
-                temp.val = val
-                if not temp.next and i < len(nums)-1:
-                    temp.next = ListNode(0)
-                temp = temp.next
-            else:
-                temp = ListNode(val)
+            temp.next = ListNode(int(nums[i]))
+            temp = temp.next
             i += 1
 
-
-        return head
+        return new_head
+        

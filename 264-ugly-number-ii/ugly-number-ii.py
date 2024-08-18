6973c855986d4ec81f7363e93d63c53308b4038a
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-08-18 10:54:58 +0530

    Time: 1109 ms (5.00%) | Memory: 12 MB (5.00%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,17 +1,9 @@
 class Solution(object):
     def nthUglyNumber(self, n):
-        lst = [1]
-        i2, i3, i5 = 0, 0, 0
-        for _ in range(1,n):
-            next_val = min(lst[i2]*2,
-                            lst[i3]*3,
-                            lst[i5]*5)
-            lst.append(next_val)
-            if next_val == lst[i2]*2:
-                i2 += 1
-            if next_val == lst[i3]*3:
-                i3 +=1
-            if next_val == lst[i5]*5:
-                i5 +=1
-
-        return lst[n-1]
+        ugly = []
+        for a in range(32):
+            for b in range(20):
+                for c in range(14):
+                    ugly.append(2**a * 3**b * 5**c)
+        ugly.sort()
+        return ugly[n-1]

951353d75d572b802eeedc63757450d7cc282314
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-08-18 10:56:11 +0530

    Time: 1928 ms (5.00%) | Memory: 12.2 MB (5.00%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -2,7 +2,7 @@ class Solution(object):
     def nthUglyNumber(self, n):
         ugly = []
         for a in range(32):
-            for b in range(20):
+            for b in range(29):
                 for c in range(14):
                     ugly.append(2**a * 3**b * 5**c)
         ugly.sort()

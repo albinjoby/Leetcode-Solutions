987a3c1c325109235ee052c1ff381d56aa9c8c13
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-08-20 11:59:25 +0530

    Time: 22 ms (9.65%) | Memory: 11.5 MB (59.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,14 +1,27 @@
 class Solution(object):
     def sequentialDigits(self, low, high):
-        vals = range(1,10)
-        lst = []
-        for i in range(len(vals)):
-            for j in range(i+1,len(vals)+1):
-                val = int(''.join(map(str, vals[i:j])))
-                if low <= val <= high:
-                    lst.append(val)
+        # vals = range(1,10)
+        # lst = []
+        # for i in range(len(vals)):
+        #     for j in range(i+1,len(vals)+1):
+        #         val = int(''.join(map(str, vals[i:j])))
+        #         if low <= val <= high:
+        #             lst.append(val)
             
-        lst.sort()
-        return lst
+        # lst.sort()
+        # return lst
+
+        res = []
+        queue = deque(range(1,10))
+        while queue:
+            n = queue.popleft()
+            if n > high:
+                continue
+            if low <= n <= high:
+                res.append(n)
+            ones = n % 10
+            if ones < 9:
+                queue.append(n * 10 + (ones+1))
+        return res
                 
                 

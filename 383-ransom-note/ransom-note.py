ed4309c1be72dbb022354b089b2eec94814cd3e7
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-09-02 19:18:05 +0530

    Time: 140 ms (5.03%) | Memory: 16.8 MB (24.18%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for letter in ransomNote:
+            if ransomNote.count(letter) > magazine.count(letter):
+                return False
+    
+        return True
+        

953a09434e2c467045b3084560d11657eb1472f4
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-09-11 21:30:53 +0530

    Time: 40 ms (22.13%) | Memory: 16.5 MB (26.96%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,16 +1,10 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        start = bin(start)[2:]
-        goal = bin(goal)[2:]
+        length = max(len(bin(start)[2:]),len(bin(goal)[2:]))
+        start = bin(start)[2:].zfill(length)
+        goal =  bin(goal)[2:].zfill(length)
         count = 0
-
-        if len(start) < len(goal):
-            while len(start) < len(goal):
-                start = '0' + start
-        if len(start) > len(goal):
-            while len(start) > len(goal):
-                goal = '0' + goal
-        
+ 
         for i in range(len(start)):
             if start[i] != goal[i]:
                 count += 1

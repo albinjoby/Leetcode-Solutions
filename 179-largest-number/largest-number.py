3e4299c71b6ebe034bfd21615a10ed28867b46da
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-09-18 12:24:23 +0530

    Time: 37 ms (79.17%) | Memory: 16.6 MB (32.99%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,16 +1,26 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        if len(nums) == 1:
-            return str(nums[0])
+        for i,num in enumerate(nums):
+            nums[i] = str(num)
 
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if int(str(nums[j])+str(nums[i])) > int(str(nums[i])+str(nums[j])):
-                    nums[i],nums[j] = nums[j],nums[i]
+        def compare(n1,n2):
+            return -1 if n1+n2 > n2+n1 else 1
 
-        for i in range(len(nums)):
-            nums[i] = str(nums[i])
+        nums = sorted(nums,key = cmp_to_key(compare))
+
+        return str(int("".join(nums)))
+
+        # if len(nums) == 1:
+        #     return str(nums[0])
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if int(str(nums[j])+str(nums[i])) > int(str(nums[i])+str(nums[j])):
+        #             nums[i],nums[j] = nums[j],nums[i]
+
+        # for i in range(len(nums)):
+        #     nums[i] = str(nums[i])
         
-        res = ''.join(nums)
+        # res = ''.join(nums)
 
-        return res if res[0] != '0' else '0'
+        # return res if res[0] != '0' else '0'

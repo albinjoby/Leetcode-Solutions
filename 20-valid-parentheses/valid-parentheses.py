42313db7a1ce7c11763630eb649c3da1fda2b02b
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-09-18 23:46:10 +0530

    Time: 33 ms (72.88%) | Memory: 16.5 MB (50.80%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {
+            '}':'{',
+            ']':'[',
+            ')':'('
+        }
+        stack = []
+        
+        for letter in s:
+            if letter in dic and stack and dic[letter] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(letter)
+        
+        return len(stack) == 0

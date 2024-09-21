3532faf96c91e497225152b399464e9092811f27
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-09-21 11:34:43 +0530

    Time: 99 ms (30.50%) | Memory: 22.8 MB (62.68%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,18 +1,22 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        res = []
-        def check(num):
-            for i in range(0,10):
-                val = num*10+i
-                if val <= n:
-                    res.append(val)
-                    check(val)
-                else:
-                    return
+        
+        check = lambda a,b : 1 if str(a) > str(b) else -1
+        return sorted([i for i in range(1,n+1)],key = cmp_to_key(check))
 
-        for num in range(1,n+1):
-            if num <= n and num not in res:
-                res.append(num)
-                check(num)
+        # res = []
+        # def check(num):
+        #     for i in range(0,10):
+        #         val = num*10+i
+        #         if val <= n:
+        #             res.append(val)
+        #             check(val)
+        #         else:
+        #             return
 
-        return res
+        # for num in range(1,n+1):
+        #     if num <= n and num not in res:
+        #         res.append(num)
+        #         check(num)
+
+        # return res

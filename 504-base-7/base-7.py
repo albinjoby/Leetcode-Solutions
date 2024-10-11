5d3dc49c6cef554c63c4307a59598b66e59b7076
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-10-11 10:07:20 +0530

    Time: 38 ms (34.71%) | Memory: 16.5 MB (82.99%) - LeetSync

--- 504-base-7/base-7.py
+++ 504-base-7/base-7.py
@@ -1,16 +1,18 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num == 0:
-            return "0"
-
-        negative = num < 0
-        num = abs(num)
-
-        res = []
-        while num != 0:
-            res.append(str(num % 7))
-            num //= 7
-
-        res = res[::-1]
-
-        return '-'+''.join(res) if negative else ''.join(res)
+        res = ''
+        if num > 0:
+            while num != 0:
+                rem = str(num % 7)
+                res += rem
+                num //= 7
+            return res[::-1]
+        elif num == 0:
+            return '0'
+        else:
+            num = -num
+            while num != 0:
+                rem = str(num % 7)
+                res += rem
+                num //= 7
+            return '-'+res[::-1]

5a33df8b918acaa05b8ec29f409ae188fd345272
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-10-13 10:31:40 +0530

    Time: 767 ms (73.18%) | Memory: 28.9 MB (5.09%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,15 +1,20 @@
+from typing import List
+
 class Solution:
     def minCost(self, colors: str, time: List[int]) -> int:
-        l = 0
+        stack = []
         res = 0
-        for r in range(1,len(colors)):
-            if colors[l] == colors[r]:
-                if time[l] < time[r]:
-                    res += time[l]
-                    l = r
+
+        for i, color in enumerate(colors):
+            if stack and stack[-1][0] == color:  # Check the last color in the stack
+                # If consecutive same-colored balloons, add the smaller cost
+                if stack[-1][1] < time[i]:
+                    res += stack[-1][1]
+                    stack.pop()
+                    stack.append((color, time[i]))  # Push the current balloon with its cost
                 else:
-                    res += time[r]
+                    res += time[i]
             else:
-                l = r
+                stack.append((color, time[i]))  # Push the color and cost to the stack
 
         return res

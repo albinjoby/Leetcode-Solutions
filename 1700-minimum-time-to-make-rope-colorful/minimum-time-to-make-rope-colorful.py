97dc1fa55a5db5732dcc102b8d327629e2145192
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-10-13 10:49:45 +0530

    Time: 829 ms (27.36%) | Memory: 25.2 MB (96.88%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,13 +1,9 @@
-from typing import List
-
 class Solution:
     def minCost(self, colors: str, time: List[int]) -> int:
-        res = 0  # Initialize result for total removal cost
-
-        for i in range(1, len(colors)):  # Start from 1 to compare with previous balloon
-            if colors[i] == colors[i - 1]:  # If the current balloon is the same color as the previous
-                res += min(time[i], time[i - 1])  # Add the smaller cost to the result
-                time[i] = max(time[i], time[i - 1])  # Keep the larger time for future comparison
+        res = 0
+        for i in range(1,len(colors)):
+            if colors[i-1] == colors[i]:
+                res += min(time[i-1],time[i])
+                time[i] = max(time[i-1],time[i])
 
         return res
-        

bc57c7c54859f486e1290f2f4d59d5496966608c
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-10-17 22:04:38 +0530

    Time: 2 ms (100.00%) | Memory: 16.7 MB (10.80%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumSwap(self, nums: int) -> int:
+        nums = list(str(nums))
+
+        max_digit = "0"
+        max_i = -1
+        swap_i = swap_j = -1
+
+        for i in reversed(range(len(nums))):
+            if nums[i] > max_digit:
+                max_digit = nums[i]
+                max_i = i
+
+            if nums[i] < max_digit:
+                swap_i, swap_j = i, max_i
+        
+        nums[swap_i], nums[swap_j] = nums[swap_j], nums[swap_i]
+
+        return int("".join(nums))

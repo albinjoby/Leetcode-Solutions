e63751d6da400f258631078958f5e603bbe4a6f6
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-10-26 20:32:29 +0530

    Time: 31 ms (91.02%) | Memory: 16.8 MB (24.78%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -2,10 +2,7 @@ class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         res = []
         for word in words:
-            if not res:
-                res.append(word)
-            else:
-                if Counter(res[-1]) != Counter(word):
-                    res.append(word)
+            if not res or Counter(res[-1]) != Counter(word):
+                res.append(word) 
 
         return res

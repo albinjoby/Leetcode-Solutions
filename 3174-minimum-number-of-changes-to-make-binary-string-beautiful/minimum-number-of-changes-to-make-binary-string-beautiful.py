0829964dc04c272c82c511feef76287492e8eb29
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-11-05 19:57:17 +0530

    Time: 23 ms (47.30%) | Memory: 16.9 MB (89.02%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -4,8 +4,8 @@ class Solution:
             return 0
 
         count = 0
-        for i in range(len(s)):
-            if i%2 == 0 and s[i] != s[i+1]:
+        for i in range(0,len(s),2):
+            if s[i] != s[i+1]:
                 count += 1
         
         return count

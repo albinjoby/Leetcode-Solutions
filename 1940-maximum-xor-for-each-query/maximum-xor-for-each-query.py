17e7d15c7d1b6b93f85aec597c8e82f3b0a72742
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2024-11-08 17:23:43 +0530

    Time: 26 ms (97.32%) | Memory: 32.2 MB (92.44%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -1,22 +1,14 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        # Calculate cumulative XOR array
-        lst = [nums[0]]
-        for i in range(1, len(nums)):
-            lst.append(lst[i-1] ^ nums[i])
+        xor = 0
+        for num in nums:
+            xor ^= num
 
-        # Maximum possible XOR value with maximumBit bits
-        max_mask = (1 << maximumBit) - 1
-
-        # Result array to store maximum XOR values
+        mask = (2**maximumBit) - 1
         res = []
-        while lst:
-            # Calculate the maximum XOR with max_mask
-            max_val = lst[-1] ^ max_mask
-            res.append(max_val)
-            
-            # Remove the last element from lst to move backward
-            lst.pop()
 
-        # Reverse result to match expected output order
+        for num in reversed(nums):
+            res.append(xor ^ mask)
+            xor ^= num
+
         return res

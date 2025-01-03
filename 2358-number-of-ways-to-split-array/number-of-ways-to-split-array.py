ce55615ee9358a8292b5412941cbdf02533e4039
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-01-03 18:29:14 +0530

    Time: 65 ms (54.12%) | Memory: 32.4 MB (51.34%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -3,8 +3,7 @@ class Solution:
         count = 0
         left,right = nums[0],sum(nums[1:])
         for i in range(1,len(nums)):
-            if left >= right:
-                count += 1
+            count += 1 if left >= right else 0
             right -= nums[i]
             left += nums[i]
         

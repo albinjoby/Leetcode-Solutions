04b151704bb27e88de542f44a5ee0500a909590a
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-04-18 20:45:49 +0530

    Time: 179 ms (69.97%) | Memory: 20.1 MB (80.90%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,20 +1,20 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        res = ''
-        i = 0
-
-        while i < len(word):
-            count = 1
-            while i < len(word)-1 and word[i] == word[i+1]:
+        comp = ""
+        count = 0
+        letter = word[0]
+        for i in range(len(word)):
+            if word[i] == letter:
                 count += 1
-                i += 1
-            while count:
-                if count >= 9:
-                    res = res + str(9) + word[i]
-                    count -= 9
-                else:
-                    res = res + str(count) + word[i]
+                if count == 9:
+                    comp += f"{count}{word[i]}"
                     count = 0
-            i += 1
+            else:
+                comp += f"{count}{letter}" if count > 0 else ""
+                count = 1
+                letter = word[i]
 
-        return res
+        comp += f"{count}{word[i]}" if count > 0 else ""
+        
+        return comp
+            

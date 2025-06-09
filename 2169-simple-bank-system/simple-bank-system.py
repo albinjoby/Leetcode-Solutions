efd257f27709de562df7829365326cd9b9310090
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-06-09 21:38:22 +0530

    Time: 45 ms (29.61%) | Memory: 48.4 MB (23.05%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.py
+++ 2169-simple-bank-system/simple-bank-system.py
@@ -3,24 +3,36 @@ class Bank:
     def __init__(self, balance: List[int]):
         self.balance = balance
         self.size = len(balance)
+        self.lock = [False] * self.size
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
         if 0 <= account1-1 < self.size and 0 <= account2-1 < self.size and self.balance[account1-1] >= money:
-            self.balance[account1-1] -= money
-            self.balance[account2-1] += money
-            return True
+            if not self.lock[account1-1] and not self.lock[account2-2]:
+                self.lock[account1-1] = True
+                self.lock[account2-1] = True
+                self.balance[account1-1] -= money
+                self.balance[account2-1] += money
+                self.lock[account1-1] = False
+                self.lock[account2-1] = False
+                return True
         return False
 
     def deposit(self, account: int, money: int) -> bool:
         if 0 <= account-1 < self.size:
-            self.balance[account-1] += money
-            return True
+            if not self.lock[account-1]:
+                self.lock[account-1] = True
+                self.balance[account-1] += money
+                self.lock[account-1] = False
+                return True
         return False
 
     def withdraw(self, account: int, money: int) -> bool:
         if 0 <= account-1 < self.size and self.balance[account-1] >= money:
-            self.balance[account-1] -= money
-            return True
+            if not self.lock[account-1]:
+                self.lock[account-1] = True
+                self.balance[account-1] -= money
+                self.lock[account-1] = False
+                return True
         return False
 
 

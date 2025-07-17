91e5fb3a646eb2769f947461a7085458099f610a
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-07-17 14:01:07 +0530

    Time: 175 ms (29.31%) | Memory: 20.7 MB (52.25%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -2,10 +2,11 @@ class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         res = []
         currentWord = ""
+        products  = sorted(products)
         for word in searchWord:
             currentWord += word
             print(currentWord)
-            curr = sorted([product for product in products if product.startswith(currentWord)])[0:3]
+            curr = [product for product in products if product.startswith(currentWord)][0:3]
             print(curr)
             res.append(curr)
         

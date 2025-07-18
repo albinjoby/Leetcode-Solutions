01ffda9a53b9320cb72e22fcba94354bc04e2624
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-07-18 08:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.70%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cache = {}
+        def check(m,n):
+            if (m,n) in cache:
+                return cache[(m,n)]
+            if m == 0 or n == 0:
+                return 0
+            if m == 1 or n == 1:
+                return 1
+            cache[(m,n)] = check(m-1,n)+check(m,n-1)
+            return cache[(m,n)]
+
+        return check(m,n)

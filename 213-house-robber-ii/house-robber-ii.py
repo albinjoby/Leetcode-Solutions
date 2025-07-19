ed52f1fcf97610c40f424b3af362ce49eedd5f88
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-07-19 14:24:09 +0530

    Time: 1 ms (16.40%) | Memory: 17.7 MB (60.24%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,10 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[-1]
+        if len(nums) == 2:
+            return max(nums[0],nums[1])
         def check(arr):
-            if len(arr) == 0:
-                return 0
-            if len(arr) == 1:
-                return arr[-1]
             dp = [0] * len(arr)
             dp[0] = arr[0]
             dp[1] = max(arr[0],arr[1])
@@ -13,4 +15,4 @@ class Solution:
 
             return dp[-1]
         
-        return max(check(nums[:-1]),check(nums[1:])) if len(nums) > 2 else max(nums)
+        return max(check(nums[:-1]),check(nums[1:]))

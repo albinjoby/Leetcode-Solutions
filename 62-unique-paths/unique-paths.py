2c88309e2e84f3375b6e4a202dce4f30a7d42352
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-07-20 11:12:54 +0530

    Time: 1 ms (31.67%) | Memory: 17.8 MB (65.36%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,14 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        cache = {}
-        def dfs(i,j):
-            if (i,j) in cache:
-                return cache[(i,j)]
-            if i == 0 or j == 0:
-                return 0
-            if i == 1 and j == 1:
-                return 1
-            cache[(i,j)] = dfs(i-1,j) + dfs(i,j-1)
-            return cache[(i,j)]
+        dp = []
+        for _ in range(m):
+            dp.append([0] * n)
 
-        return dfs(m,n)
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0: continue
+                dp[i][j] = (dp[i-1][j] if i > 0 else 0) + (dp[i][j-1] if j > 0 else 0)
+
+        print(dp)
+        return dp[m-1][n-1]
+
+        # cache = {}
+        # def dfs(i,j):
+        #     if (i,j) in cache:
+        #         return cache[(i,j)]
+        #     if i == 0 or j == 0:
+        #         return 0
+        #     if i == 1 and j == 1:
+        #         return 1
+        #     cache[(i,j)] = dfs(i-1,j) + dfs(i,j-1)
+        #     return cache[(i,j)]
+
+        # return dfs(m,n)

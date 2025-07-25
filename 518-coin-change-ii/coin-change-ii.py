954fee20e89c166b1e99dbc5f250074b89fd6bfa
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-07-25 23:04:38 +0530

    Time: 1303 ms (5.07%) | Memory: 274.3 MB (9.99%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo = {}
+
+        def dfs(i,a):
+            if (i,a) in memo:
+                return memo[(i,a)]
+            if a == amount:
+                return 1
+            if a > amount or i == len(coins):
+                return 0
+            memo[(i,a)] = dfs(i, a + coins[i]) + dfs(i+1, a)
+            return memo[(i,a)]
+        
+        return dfs(0,0)

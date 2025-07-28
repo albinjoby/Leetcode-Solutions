7434a4b9f4ff674c4d92e389f5af117c3421f3fe
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-07-28 21:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.54%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,22 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        l = 0
+        r = len(matrix[0])-1
+        t = 0
+        b = len(matrix)-1
         res = []
-        left, right, top, bottom = 0, len(matrix[0])-1, 0, len(matrix)-1
-
-        while left <= right and top <= bottom:
-            for i in range(left,right+1):
-                res.append(matrix[top][i])
-            top += 1
-            for j in range(top,bottom+1):
-                res.append(matrix[j][right])
-            right -= 1
-            if not(left <= right and top <= bottom):
-                break
-            for i in range(right,left-1,-1):
-                res.append(matrix[bottom][i])
-            bottom -= 1
-            for j in range(bottom,top-1,-1):
-                res.append(matrix[j][left])
-            left += 1
+        while l<=r and t<=b:
+            for i in range(l,r+1):
+                res.append(matrix[t][i])
+            t += 1
+            for j in range(t,b+1):
+                res.append(matrix[j][r])
+            r -= 1
+            if t<=b:
+                for i in range(r,l-1,-1):
+                    res.append(matrix[b][i])
+                b -= 1
+            if l<=r:
+                for j in range(b,t-1,-1):
+                    res.append(matrix[j][l])
+                l += 1
 
         return res

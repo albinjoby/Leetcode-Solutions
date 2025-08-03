fdad5a23b251be9492fbc171b08dfa71450132a5
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-08-03 10:45:09 +0530

    Time: 24 ms (15.49%) | Memory: 20.1 MB (17.26%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -2,14 +2,6 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         res = [1] * len(ratings)
         print(res)
-        # if len(ratings) > 1 and ratings[0] > ratings[1]:
-        #     res[0] += 1
-        # if len(ratings) > 1 and ratings[-1] > ratings[-2]:
-        #     res[-1] += 1
-        # for i in range(1,len(ratings)-1):
-        #     if ratings[i-1] < ratings[i] or ratings[i] > ratings[i+1]:
-        #         res[i] += 1
-        # print(res)
 
         for i in range(1,len(ratings)):
             if ratings[i-1] < ratings[i]:

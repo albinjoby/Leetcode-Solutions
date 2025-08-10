b6e717876e9c9c8b45c126f13753cfdb555180d1
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-08-10 19:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (35.50%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,7 +1,7 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         target = sorted(str(n))
-        for i in range(31):
+        for i in range(32):
             if sorted(str(1<<i)) == target:
                 return True
         return False

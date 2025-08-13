9c6e8e3f039720c5e7f949469284068a42329fcf
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-08-13 20:02:01 +0530

    Time: 176 ms (20.00%) | Memory: 59.7 MB (55.63%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -4,6 +4,7 @@ class Solution:
         def backtrack(idx,path):
             if len(path) == k:
                 res.append(path[:])
+                return
             for num in range(idx,n+1):
                 path.append(num)
                 backtrack(num+1,path[:])

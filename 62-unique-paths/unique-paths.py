c925c222213fb3a83826101057e41d8465ce3d68
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-08-18 13:40:43 +0530

    Time: 2 ms (25.69%) | Memory: 18 MB (26.08%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,30 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # bottom up with tabulation
-
-        dp = []
-        for _ in range(m):
-            dp.append([0] * n)
-
-        dp[0][0] = 1
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0: continue
-                dp[i][j] = (dp[i-1][j] if i > 0 else 0) + (dp[i][j-1] if j > 0 else 0)
-
-        return dp[m-1][n-1]
-
-        # top down with memorization
-                
-        # cache = {}
-        # def dfs(i,j):
-        #     if (i,j) in cache:
-        #         return cache[(i,j)]
-        #     if i == 0 or j == 0:
-        #         return 0
-        #     if i == 1 and j == 1:
-        #         return 1
-        #     cache[(i,j)] = dfs(i-1,j) + dfs(i,j-1)
-        #     return cache[(i,j)]
-
-        # return dfs(m,n)
+        # recursion using memorization
+        memo={(0,0):0}
+        def dp(i,j):
+            if i <= 0 or j <= 0:
+                return 1
+            if (i,j) in memo:
+                return memo[(i,j)]
+            memo[(i,j)] = dp(i-1,j) + dp(i,j-1)
+            return memo[(i,j)]
+        res = dp(m-1,n-1)
+        print(memo)
+        return res

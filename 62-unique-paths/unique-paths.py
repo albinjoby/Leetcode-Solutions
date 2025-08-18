c0783563cdad619252ee0cb57cccbdf27ae5a7a1
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-08-18 14:06:28 +0530

    Time: 3 ms (21.21%) | Memory: 17.7 MB (65.14%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,14 +1,24 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         # recursion using memorization
-        memo={(0,0):0}
-        def dp(i,j):
-            if i <= 0 or j <= 0:
-                return 1
-            if (i,j) in memo:
-                return memo[(i,j)]
-            memo[(i,j)] = dp(i-1,j) + dp(i,j-1)
-            return memo[(i,j)]
-        res = dp(m-1,n-1)
-        print(memo)
-        return res
+        # memo={(0,0):0}
+        # def dp(i,j):
+        #     if i <= 0 or j <= 0:
+        #         return 1
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+        #     memo[(i,j)] = dp(i-1,j) + dp(i,j-1)
+        #     return memo[(i,j)]
+        # return dp(m-1,n-1)
+
+        # iteration using tabulation
+        dp = [[0]*n for _ in range(m)]
+        dp[0][0] = 1
+        print(dp)
+        for i in range(m):
+            for j in range(n):
+                if j-1 >= 0:
+                    dp[i][j] += dp[i][j-1]
+                if i-1 >= 0:
+                    dp[i][j] += dp[i-1][j] 
+        return dp[m-1][n-1]

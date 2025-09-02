114838b9e6082ef0cb13739495787a25ce3b833e
Albin_Jo <127176998+albinjoby@users.noreply.github.com>
2025-09-02 09:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (61.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,18 +1,27 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        heat_map = [0]*len(nums)
-        for i in range(1,len(nums)):
-            if nums[i] > nums[i-1]:
-                heat_map[i] = heat_map[i-1] + 1
-        print(heat_map)
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i] > nums[i+1]:
-                heat_map[i] = heat_map[i] + heat_map[i+1]
-        print(heat_map)
-        max_val,max_idx = 0,0
-        for i in range(len(heat_map)):
-            if heat_map[i] > max_val:
-                max_val = heat_map[i]
-                max_idx = i            
+        l,r = 0,len(nums)-1
+        while l < r:
+            m = (l+r)//2
+            if nums[m] < nums[m+1]:
+                l = m+1
+            else:
+                r = m
+        return l
+
+        # heat_map = [0]*len(nums)
+        # for i in range(1,len(nums)):
+        #     if nums[i] > nums[i-1]:
+        #         heat_map[i] = heat_map[i-1] + 1
+        # print(heat_map)
+        # for i in range(len(nums)-2,-1,-1):
+        #     if nums[i] > nums[i+1]:
+        #         heat_map[i] = heat_map[i] + heat_map[i+1]
+        # print(heat_map)
+        # max_val,max_idx = 0,0
+        # for i in range(len(heat_map)):
+        #     if heat_map[i] > max_val:
+        #         max_val = heat_map[i]
+        #         max_idx = i            
         
-        return max_idx
+        # return max_idx
